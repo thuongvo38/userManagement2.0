0549a04952be51e8cf764229a56a2417e4d49ecc
vothuong <thuongvo24112002@gmail.com>
2024-05-23 03:24:07 +0700

    static file

--- src/sever.js
+++ src/sever.js
@@ -6,21 +6,24 @@ console.log(process.env); // remove this after you've confirmed it is working
 const app = express(); // tao express application
 const port = process.env.PORT || 8080; // init port vs env
 const hostname = process.env.HOST_NAME;
-//khai báo routes
 
 //config template engine this two elements
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//config static files
+app.use(express.static(path.join(__dirname, "public"))); // đoạn này khai báo hơi khác so với document là bỏ ' static ' đi & public nghĩa là tên file public đó
+
+//khai báo routes
 app.get("/", (req, res) => {
-  res.send("Hello World! chao ban");
+  res.send("Hello World! chao ban cung voi nodemon");
 });
 
 app.get("/abc", (req, res) => {
   res.send("day la trang theo duong dan abc");
 });
 
-app.get("/templates", (req, res) => {
+app.get("/hoidanit", (req, res) => {
   res.render("sample.ejs");
 });
 
