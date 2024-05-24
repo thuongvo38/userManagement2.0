69a5dc96b3ddec798790a0bc8fc7061820a0685a
vothuong <thuongvo24112002@gmail.com>
2024-05-25 03:49:54 +0700

    config view ejs

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,13 +1,8 @@
 const path = require("path");
-const express = require("express");
-const app = express();
-
-const viewEngine = (app) => {
+const configViewEngine = (app) => {
   //config template engine this two elements
-  console.log(__dirname);
-  app.set("views", path.join("./src", "views")); // views first la name, views sau la value -> noi dan link
+  app.set("views", path.join(__dirname, "views"));
   app.set("view engine", "ejs");
-  app.use(express.static(path.join("./src", "public"))); // đoạn này khai báo hơi khác so với document là bỏ ' static ' đi & public nghĩa là tên file public đó
 };
 
-module.exports = viewEngine;
+module.exports = configViewEngine;
